2698dae392a17fbab13e27ef9ac17152d8e2fd00
Evgen003 <160235473+Evgen003@users.noreply.github.com>
2024-02-21 21:25:27 +0300

    Add files via upload

---
+++ glm/out/build/x64-Debug/glm/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/My_projects/Graf/AOKG/Lab_10/glm/glm
+# Build directory: D:/My_projects/Graf/AOKG/Lab_10/glm/out/build/x64-Debug/glm
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
