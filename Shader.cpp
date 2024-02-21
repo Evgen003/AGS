718e0b3cd3f3be4070247e2e0be9b043e7c36bb0
Evgen003 <evgeny.44ir@yandex.ru>
2024-02-21 22:46:30 +0300

    a

--- Shader.cpp
+++ Shader.cpp
@@ -52,7 +52,6 @@ GLuint Shader::createShaderObject(GLenum shaderType, std::string filename) {
 	else {
 		cout << filename << " open success\n";
 	}
-	
 	file.getline(buffer, max_buffer_size, 0);
 	GLint shader = glCreateShader(shaderType);
 	if (shader == 0) {
@@ -61,7 +60,7 @@ GLuint Shader::createShaderObject(GLenum shaderType, std::string filename) {
 	}
 	const char* source_pointer[] = { buffer };
 	glShaderSource(shader, 1, source_pointer, 0);
-	//cout << buffer;
+	cout << buffer << endl;
 	glCompileShader(shader);
 	GLint compileStatus;
 	glGetShaderiv(shader, GL_COMPILE_STATUS, &compileStatus);
