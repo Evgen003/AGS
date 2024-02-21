1d8e98efba9ce4c1ff1e89708373ec20fd7d0ab8
Evgen003 <evgeny.44ir@yandex.ru>
2024-02-21 22:54:53 +0300

    a

--- Data.cpp
+++ Data.cpp
@@ -26,44 +26,58 @@ void initData() {
 // функци€ дл€ вывода квадрата с ребрами равными единице (от -0.5 до +0.5)
 void drawObject()
 {
-	// переменные дл€ вывода объекта (пр€моугольника из двух треугольников)
-	static bool init = true;
-	static GLuint VAO_Index = 0;		// индекс VAO-буфера
-	static GLuint VBO_Index = 0;		// индекс VBO-буфера
-	static int VertexCount = 0;			// количество вершин
+	{
+		// переменные дл€ вывода объекта (пр€моугольника из двух треугольников)
+		static GLuint VAO_Index = 0;	// индекс VAO-буфера
+		static GLuint VBO_Index = 0;	// индекс VBO-буфера
+		static int VertexCount = 0;		// количество вершин
+		static bool init = true;
 
-	// при первом вызове инициализируем VBO и VAO
-	if (init) {
-		init = false;
-		// создание и заполнение VBO
-		glGenBuffers(1, &VBO_Index);
-		glBindBuffer(GL_ARRAY_BUFFER, VBO_Index);
-		GLfloat	Verteces[] = {
+		if (init) {
+			// создание и заполнение VBO
+			glGenBuffers(1, &VBO_Index);
+			glBindBuffer(GL_ARRAY_BUFFER, VBO_Index);
+			GLfloat Verteces[] = {
+				// передн€€ грань (два треугольника)
+				-0.5, +0.5, +0.5, -0.5, -0.5, +0.5, +0.5, +0.5, +0.5,
+				+0.5, +0.5, +0.5, -0.5, -0.5, +0.5, +0.5, -0.5, +0.5,
+				// задн€€ грань (два треугольника)
+				+0.5, +0.5,	-0.5, +0.5, -0.5, -0.5, -0.5, +0.5, -0.5,
+				-0.5, +0.5,	-0.5, +0.5, -0.5, -0.5, -0.5, -0.5, -0.5,
+				// права€ грань (два треугольника) 
+				+0.5, -0.5,	+0.5, +0.5, -0.5, -0.5, +0.5, +0.5, +0.5,
+				+0.5, +0.5,	+0.5, +0.5, -0.5, -0.5, +0.5, +0.5, -0.5,
+				// лева€ грань (два треугольника)
+				-0.5, +0.5,	+0.5, -0.5, +0.5, -0.5, -0.5, -0.5, +0.5,
+				-0.5, -0.5,	+0.5, -0.5, +0.5, -0.5, -0.5, -0.5, -0.5,
+				// верхн€€ грань (два треугольника)
+				-0.5, +0.5, -0.5, -0.5, +0.5, +0.5, +0.5, +0.5, -0.5,
+				+0.5, +0.5, -0.5, -0.5, +0.5, +0.5, +0.5, +0.5, +0.5,
+				// нижн€€ грань (два треугольника)
+				-0.5, -0.5, +0.5, -0.5, -0.5, -0.5, +0.5, -0.5, +0.5,
+				+0.5, -0.5, +0.5, -0.5, -0.5, -0.5, +0.5, -0.5, -0.5
+			};
+			glBufferData(GL_ARRAY_BUFFER, sizeof(Verteces), Verteces, GL_STATIC_DRAW);
 
-			-0.5, +0.5,
-			-0.5, -0.5,
-			+0.5, +0.5,
-			+0.5, +0.5,
-			-0.5, -0.5,
-			+0.5, -0.5
-		};
-		glBufferData(GL_ARRAY_BUFFER, sizeof(Verteces), Verteces, GL_STATIC_DRAW);
+			// создание VAO
+			glGenVertexArrays(1, &VAO_Index);
+			glBindVertexArray(VAO_Index);
 
-		// создание VAO
-		glGenVertexArrays(1, &VAO_Index);
+			// инициализаци€ VAO
+			glBindBuffer(GL_ARRAY_BUFFER, VBO_Index);
+			int location = 0;
+			glVertexAttribPointer(location, 3, GL_FLOAT, GL_FALSE, 0, 0);
+			glEnableVertexAttribArray(location);
+
+			// "отв€зка" буфера VAO на вс€кий случай, чтоб случайно не испортить
+			glBindVertexArray(0);
+
+			// указание количество вершин
+			VertexCount = 6 * 6;
+			init = false;
+		}
+
+		// вывод модели кубика на экран
 		glBindVertexArray(VAO_Index);
-		// заполнение VAO
-		glBindBuffer(GL_ARRAY_BUFFER, VBO_Index);
-		glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0);
-		glEnableVertexAttribArray(0);
-		// "отв€зка" буфера VAO, чтоб случайно не испортить
-		glBindVertexArray(0);
-
-		// указание количество вершин
-		VertexCount = 3;
-	}
-
-	// выводим пр€моугольник
-	glBindVertexArray(VAO_Index);
-	glDrawArrays(GL_TRIANGLES, 0, 8);
+		glDrawArrays(GL_TRIANGLES, 0, VertexCount);
 }
